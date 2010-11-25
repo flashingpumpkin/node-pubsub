b2a6aac66824ba0737c6b237146e63ef28ab018a
Alen Mujezinovic <flashingpumpkin@gmail.com>
2010-11-25 11:04:13 +0000

    Updated test for multiple publishers

--- test/test.coffee
+++ test/test.coffee
@@ -4,27 +4,42 @@ assert = require 'assert'
 conn = pubsub.createConnection host: 'localhost'
 conn.on 'ready', ->
     
-    publisher = conn.createPublisher('test-publisher')
+    publisher1 = conn.createPublisher('test-publisher')
+    publisher2 = conn.createPublisher('test-publisher-2')
 
     subscriber1 = conn.createSubscriber('test-publisher')
     subscriber2 = conn.createSubscriber('test-publisher')
 
-    subscriberMessageCount = 0
+    subscriber3 = conn.createSubscriber('test-publisher-2')
+    subscriber4 = conn.createSubscriber('test-publisher-2')
+
+    subscriberMessageCount1 = 0
+    subscriberMessageCount2 = 0
 
     subscriber1.on 'message', ->
-        subscriberMessageCount++
+        subscriberMessageCount1++
 
     subscriber2.on 'message', ->
-        subscriberMessageCount++
+        subscriberMessageCount1++
+
+    subscriber3.on 'message', ->
+        subscriberMessageCount2++
+
+    subscriber4.on 'message', ->
+        subscriberMessageCount2++
 
     setTimeout (->
-        publisher.publish('test')
-        publisher.publish('test')
-        publisher.publish('test')
+        publisher1.publish('test')
+        publisher1.publish('test')
+        publisher1.publish('test')
+
+        publisher2.publish('test')
+        publisher2.publish('test')
+        publisher2.publish('test')
     ),200
 
     setTimeout (->
-        assert.equal subscriberMessageCount, 6
+        assert.equal subscriberMessageCount1, 6
+        assert.equal subscriberMessageCount2, 6
         console.log 'Test passed'
-        process.exit 0
     ),1000
--- test/test.js
+++ test/test.js
@@ -6,26 +6,39 @@
     host: 'localhost'
   });
   conn.on('ready', function() {
-    var publisher, subscriber1, subscriber2, subscriberMessageCount;
-    publisher = conn.createPublisher('test-publisher');
+    var publisher1, publisher2, subscriber1, subscriber2, subscriber3, subscriber4, subscriberMessageCount1, subscriberMessageCount2;
+    publisher1 = conn.createPublisher('test-publisher');
+    publisher2 = conn.createPublisher('test-publisher-2');
     subscriber1 = conn.createSubscriber('test-publisher');
     subscriber2 = conn.createSubscriber('test-publisher');
-    subscriberMessageCount = 0;
+    subscriber3 = conn.createSubscriber('test-publisher-2');
+    subscriber4 = conn.createSubscriber('test-publisher-2');
+    subscriberMessageCount1 = 0;
+    subscriberMessageCount2 = 0;
     subscriber1.on('message', function() {
-      return subscriberMessageCount++;
+      return subscriberMessageCount1++;
     });
     subscriber2.on('message', function() {
-      return subscriberMessageCount++;
+      return subscriberMessageCount1++;
+    });
+    subscriber3.on('message', function() {
+      return subscriberMessageCount2++;
+    });
+    subscriber4.on('message', function() {
+      return subscriberMessageCount2++;
     });
     setTimeout((function() {
-      publisher.publish('test');
-      publisher.publish('test');
-      return publisher.publish('test');
+      publisher1.publish('test');
+      publisher1.publish('test');
+      publisher1.publish('test');
+      publisher2.publish('test');
+      publisher2.publish('test');
+      return publisher2.publish('test');
     }), 200);
     return setTimeout((function() {
-      assert.equal(subscriberMessageCount, 6);
-      console.log('Test passed');
-      return process.exit(0);
+      assert.equal(subscriberMessageCount1, 6);
+      assert.equal(subscriberMessageCount2, 6);
+      return console.log('Test passed');
     }), 1000);
   });
 }).call(this);
